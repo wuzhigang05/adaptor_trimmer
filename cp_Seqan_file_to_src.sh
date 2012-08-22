4f69edbca0a424db5295ecd359748b256951260d
wuzhigang05 <zhigangwu.bgi@gmail.com>
2012-08-22 14:32:59 -0700

    update the cp_Seqan_file_to_src.sh 3

--- cp_Seqan_file_to_src.sh
+++ cp_Seqan_file_to_src.sh
@@ -4,4 +4,5 @@ mkdir SeqAn1.3/seqan
 for i in `get_what_Seqan_file_used.sh seqan`; do cp -r ~/software/Cplusplus_libs/seqan-trunk/core/include/$i* SeqAn1.3/seqan/; done
 cp ~/software/Cplusplus_libs/seqan-trunk/LICENSE SeqAn1.3
 cp ~/software/Cplusplus_libs/seqan-trunk/README SeqAn1.3
-cp -r /Users/kaitang/software/Cplusplus_libs/seqan-trunk//core/include/seqan/platform* SeqAn1.3/seqan/
+cp -r /Users/kaitang/software/Cplusplus_libs/seqan-trunk/core/include/seqan/platform* SeqAn1.3/seqan/
+cp -r /Users/kaitang/software/Cplusplus_libs/seqan-trunk/core/include/seqan/misc* SeqAn1.3/seqan/
